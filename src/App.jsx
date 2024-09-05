47158821e400cbd96e7c72cf9a753ae1c2e9af12
Pallavi <pallavishettigar03@gmail.com>
2024-09-05 19:51:32 +0530

    add changes

--- src/App.jsx
+++ src/App.jsx
@@ -1,7 +1,17 @@
 
 import "./styles.css"
 export default function App(){
-  return <form class="new-item-form">
+  return( 
+  <><form className="new-item-form">
+  <div className="form-row">
+    <label htmlFor="item"> New Item</label>
+    <input type="text" id="item"/>
 
+  </div>
+  <button className="btn">Add </button>
   </form>
+  <h1 className="header"> To do List</h1>
+  </>
+  )
+
 }
