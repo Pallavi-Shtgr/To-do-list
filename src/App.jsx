28a1af8d26c1b5492febe445f5874b8a7377ed4f
Pallavi <pallavishettigar03@gmail.com>
2024-09-16 22:02:09 +0530

    add changes

--- src/App.jsx
+++ src/App.jsx
@@ -35,21 +35,11 @@ export default function App() {
 
   return (
     <>
-      <form onSubmit={handleSubmit} className="new-item-form">
-        <div className="form-row">
-          <label htmlFor="item">New Item</label>
-          <input
-            value={newItem}
-            onChange={(e) => setNewItem(e.target.value)}
-            type="text"
-            id="item"
-          />
-        </div>
-        <button className="btn">Add</button>
-      </form>
+     
 
       <h1 className="header">To Do List</h1>
       <ul className="list">
+      {todo.length==0 && "No Todos"}
         {todos.map((todo) => (
           <li key={todo.id}>
             <label>
